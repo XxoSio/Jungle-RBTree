10a3f9e221c713b110fba67a5ccbc869788792be
XxoSio <ghdtjgml7088@gmail.com>
2022-11-27 05:13:26 +0900

    new and insert

--- src/rbtree.h
+++ src/rbtree.h
@@ -1,7 +1,9 @@
 #ifndef _RBTREE_H_
 #define _RBTREE_H_
 
+#include <stdio.h>
 #include <stddef.h>
+#include <stdlib.h>
 
 typedef enum { RBTREE_RED, RBTREE_BLACK } color_t;
 
@@ -18,15 +20,23 @@ typedef struct {
   node_t *nil;  // for sentinel
 } rbtree;
 
+
+// void delete_rbtree(rbtree *);
+
+// node_t *rbtree_find(const rbtree *, const key_t);
+// node_t *rbtree_min(const rbtree *);
+// node_t *rbtree_max(const rbtree *);
+// int rbtree_erase(rbtree *, node_t *);
+
+// int rbtree_to_array(const rbtree *, key_t *, const size_t);
+
+// --------------------------------------------------------------------
 rbtree *new_rbtree(void);
-void delete_rbtree(rbtree *);
 
 node_t *rbtree_insert(rbtree *, const key_t);
-node_t *rbtree_find(const rbtree *, const key_t);
-node_t *rbtree_min(const rbtree *);
-node_t *rbtree_max(const rbtree *);
-int rbtree_erase(rbtree *, node_t *);
+node_t *rbtree_insert_fixup(rbtree *t, node_t *z);
+node_t *left_rotate(rbtree *t, node_t *z);
+node_t *right_rotate(rbtree *t, node_t *z);
 
-int rbtree_to_array(const rbtree *, key_t *, const size_t);
 
 #endif  // _RBTREE_H_
