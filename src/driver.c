afc5de609ff3c530dcacdaba4dcf56b01f4ca25b
XxoSio <ghdtjgml7088@gmail.com>
2022-11-27 22:44:52 +0900

    commit

--- src/driver.c
+++ src/driver.c
@@ -1,13 +1,28 @@
 #include "rbtree.h"
+#include <stdio.h>
+
+// void inorder(rbtree *t, node_t *start)
+// {
+//   if(start == t->nil)
+//   {
+//     return;
+//   }
+
+//   inorder(t, start->left);
+//   printf("%d", start->key);
+//   inorder(t, start->right);
+
+//   return;
+// }
 
 int main(int argc, char *argv[]) {
-        rbtree *t = new_rbtree();
+  rbtree *t = new_rbtree();
 
-        rbtree_insert(t, 10);
-        rbtree_insert(t, 20);
-        rbtree_insert(t, 30);
+  rbtree_insert(t, 10);
+  rbtree_insert(t, 20);
+  rbtree_insert(t, 30);
 
-        printf("HelloHelloHelloHelloHelloHello2\n");
+  // inorder(t, t->root);
 
-        return 0;
+  return 0;
 }
